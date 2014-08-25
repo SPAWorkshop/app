31f5c05dd50445fd4eeea0e76b17bbe536bf0ab6
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2014-08-26 01:00:25 +0200

    hide sqlite file by default

--- backend/lightningtalks/settings.py
+++ backend/lightningtalks/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'lightningtalks.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '.db.sqlite3'),
     }
 }
 
