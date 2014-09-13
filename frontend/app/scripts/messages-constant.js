76379e985cf8fa37031b01ef2860a0b662f33991
konradhalas <halas.konrad@gmail.com>
2014-09-13 20:39:26 +0200

    Talk delete.

--- frontend/app/scripts/messages-constant.js
+++ frontend/app/scripts/messages-constant.js
@@ -4,6 +4,7 @@ angular.module('lightningtalks')
   .constant('messages', {
     REGISTRATION_SUCCESS: 'Registration complete - you could now sign in.',
     TALK_CREATE_SUCCESS: 'Talk added successfully.',
+    TALK_DELETE_SUCCESS: 'Talk deleted successfully.',
     LOGIN_SUCCESS: 'You are logged in.',
     LOGOUT_SUCCESS: 'You are logged out.'
   });
--- frontend/app/scripts/talk-list-ctrl.js
+++ frontend/app/scripts/talk-list-ctrl.js
@@ -1,9 +1,20 @@
 'use strict';
 
 angular.module('lightningtalks')
-  .controller('TalkListCtrl', function ($scope, $resource, $location, auth) {
+  .controller('TalkListCtrl', function ($scope, $resource, $location, auth, flash, messages) {
 
     auth.shouldBeLoggedIn();
 
-    $scope.talks = $resource('http://127.0.0.1:8000/api/talks').query();
+    var fetch = function () {
+      $scope.talks = $resource('http://127.0.0.1:8000/api/talks').query();
+    };
+
+    fetch();
+
+    $scope.delete = function (talk) {
+      $resource('http://127.0.0.1:8000/api/talks/:id', {id: talk.id}).delete().$promise.then(function () {
+        flash(messages.TALK_DELETE_SUCCESS);
+        fetch();
+      });
+    };
   });
