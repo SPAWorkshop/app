e42ca2e83682f824c677cd1032a5f3897a886243
konradhalas <halas.konrad@gmail.com>
2014-09-13 17:01:37 +0200

    Update talks app models.

--- backend/talks/models.py
+++ backend/talks/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from apiauth.models import User
 
 
 class Session(models.Model):
     name = models.CharField(max_length=64, default='Lightning Talks')
     starts_at = models.DateTimeField()
-    max_slots = models.PositiveSmallIntegerField(default=10)
-    default_slot_time = models.PositiveSmallIntegerField(default=5)
+    max_talks = models.PositiveSmallIntegerField(default=10)
+    talk_time = models.PositiveSmallIntegerField(default=5)
 
     def __str__(self):
         return self.name
@@ -15,12 +16,8 @@ class Session(models.Model):
 class Talk(models.Model):
     session = models.ForeignKey(Session, related_name='talks')
     title = models.CharField(max_length=256)
-    author = models.CharField(max_length=128)
+    author = models.ForeignKey(User)
     created_at = models.DateTimeField(default=timezone.now)
-    number = models.PositiveSmallIntegerField()
-
-    class Meta:
-        unique_together = ('session', 'number')
 
     def __str__(self):
         return self.title
