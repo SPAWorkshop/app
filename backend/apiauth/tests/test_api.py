fc2370488b7850114364aa4f040226642ff46bad
~/.gitconfig <lukaszbalcerzak@gmail.com>
2014-09-13 13:59:54 +0200

    /register endpoint

--- backend/apiauth/tests/test_api.py
+++ backend/apiauth/tests/test_api.py
@@ -1,3 +1,4 @@
+from apiauth.models import User
 from django.core.urlresolvers import reverse
 from rest_framework.test import APITestCase
 
@@ -10,54 +11,71 @@ class TestRegisterToLoginFlow(APITestCase):
     def test_register(self):
         """
         - send email and password to /register
-        - expect HTTP 201 (from the client point of view it creates a
-          activation token)
-        - ensure we email is sent with activation link
+        - expect HTTP 201 (user is created)
+        - ensure user is created
+        - ensure auth token is assigned to created user
         """
+        response = self.client.post(self.url, {
+            'email': 'joe@doe.com',
+            'password': 's3cr3t',
+        })
+
+        self.assertEqual(response.status_code, 201)
+        user = User.objects.get(email='joe@doe.com')
+        self.assertTrue(user.check_password('s3cr3t'))
 
     def test_register_user_exists(self):
         """
         - send email and password to /register
         - expect HTTP 400 (cannot register user that already exists)
+        - expect 'email' in returned json
         """
+        User.objects.create_user('joe@doe.com', 's3cr3t')
+
+        response = self.client.post(self.url, {
+            'email': 'joe@doe.com',
+            'password': 's3cret3',
+        })
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email', response.data)
 
     def test_register_wrong_email(self):
         """
         - send email and password to /register but with malformed email value
         - expect HTTP 400
+        - expect 'email' in returned json
         """
+        response = self.client.post(self.url, {
+            'email': 'joe@doe',
+            'password': 's3cret3',
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email', response.data)
 
+    def test_register_missing_email(self):
+        """
+        - send password to /register (no email)
+        - expect HTTP 400
+        - expect 'email' in returned json
+        """
+        response = self.client.post(self.url, {
+            'password': 's3cret3',
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email', response.data)
 
-class TestActivate(APITestCase):
-
-    def setUp(self):
-        self.url = reverse('activate')
-
-    def test_activate(self):
+    def test_register_missing_password(self):
         """
-        - create user and activation token
-        - send token value to /activate
-        - expect HTTP 201 (from the client point of view it creates an user)
-        - ensure user.is_active
+        - send email to /register (no password)
+        - expect HTTP 400
+        - expect 'password' in returned json
         """
-
-    def test_activate_wrong_token(self):
-        """
-        - create user and activation token
-        - send wrong token value to /activate
-        - expect HTTP 201 (from the client point of view it creates an user)
-        - ensure user.is_active
-        - ensure activation token is not reusable (send token again and expect
-          HTTP 400)
-        """
-
-    def test_activate_reuse_token(self):
-        """
-        - create inactive user
-        - create activation token for that user and mark it as already used
-        - send token value to /activate
-        - expect HTTP 400 ("This user was already activated")
-        """
+        response = self.client.post(self.url, {
+            'email': 'joe@doe.com',
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('password', response.data)
 
 
 class TestLogin(APITestCase):
--- backend/apiauth/views.py
+++ backend/apiauth/views.py
@@ -1,8 +1,19 @@
+from . import models
+from . import serializers
 from django.http import JsonResponse
+from rest_framework import generics
+from rest_framework import permissions
 
 
-def register(request):
-    return JsonResponse({})
+class RegistrationView(generics.CreateAPIView):
+    http_method_names = ['post', 'options']
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializers.UserCreateSerializer
+
+    #def post_save(self, obj, created):
+        #auth_models.Token.objects.get_or_create(user=obj)
+
+register = RegistrationView.as_view()
 
 
 def activate(request):
