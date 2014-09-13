4cebc9fdf1e93c84b605fd624053e91654909fb5
konradhalas <halas.konrad@gmail.com>
2014-09-13 17:03:18 +0200

    Fix login ctrl.

--- frontend/app/scripts/login-ctrl.js
+++ frontend/app/scripts/login-ctrl.js
@@ -3,7 +3,7 @@
 angular.module('lightningtalks')
   .controller('LoginCtrl', function ($scope, $resource, $location, auth) {
     $scope.submit = function () {
-      var response = $resource('http://127.0.0.1:8000/api-auth/login').save($scope.user).$promise.then(function () {
+      $resource('http://127.0.0.1:8000/api-auth/login').save($scope.user).$promise.then(function (response) {
         auth.login(response.token);
         $location.path('/');
       });
