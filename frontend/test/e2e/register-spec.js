d22931470959b8e395500d9c6b6c38cf4db50073
~/.gitconfig <lukaszbalcerzak@gmail.com>
2014-09-14 16:12:08 +0200

    e2e /register test

--- frontend/test/e2e/register-spec.js
+++ frontend/test/e2e/register-spec.js
@@ -25,14 +25,20 @@ describe('The main view', function () {
         var email = element(by.model('user.email'));
         var password = element(by.model('user.password'));
 
+        var emailErrors = element.all(by.repeater('error in errors.email'));
+        var passwordErrors = element.all(by.repeater('error in errors.password'));
+
         // TODO: send some random email (otherwiser we might try to register user
         // with already taken email which would end up with an error)
-        email.sendKeys('joe21@doe.com');
-        email.sendKeys('mypass');
+        var userId = (new Date()).getTime();
+        email.sendKeys('joe' + userId + '@doe.com');
+        password.sendKeys('mypass');
 
         element(by.css('.register-button')).click().then(function(){
+            expect(emailErrors.count()).toEqual(0);
+            expect(passwordErrors.count()).toEqual(0);
             // TODO: fix test below (check browser API)
-            expect(browser.getLocationAbsUrl()).toBe('/#/login');
+            expect(browser.getLocationAbsUrl()).toMatch('/#/login');
         });
     });
 
