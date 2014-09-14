05aff66a0c38829dd13e4c8063ae7574d386f7da
~/.gitconfig <lukaszbalcerzak@gmail.com>
2014-09-14 14:09:06 +0200

    updated login and registration api calls at UI

--- frontend/app/scripts/auth/login-ctrl.js
+++ frontend/app/scripts/auth/login-ctrl.js
@@ -4,7 +4,7 @@ angular.module('lightningtalks')
   .controller('LoginCtrl', function ($scope, $resource, $location, auth, toasty, messages) {
     $scope.submit = function () {
       $scope.inProgress = true;
-      $resource('http://127.0.0.1:8000/api-auth/login').save($scope.user).$promise.then(function (response) {
+      $resource('http://127.0.0.1:8000/api/auth/login').save($scope.user).$promise.then(function (response) {
         auth.login(response.token);
         toasty.pop.success(messages.LOGIN_SUCCESS);
         $scope.inProgress = false;
--- frontend/app/scripts/auth/registration-ctrl.js
+++ frontend/app/scripts/auth/registration-ctrl.js
@@ -4,7 +4,7 @@ angular.module('lightningtalks')
   .controller('RegistrationCtrl', function ($scope, $resource, $location, toasty, messages) {
     $scope.submit = function () {
       $scope.inProgress = true;
-      $resource('http://127.0.0.1:8000/api-auth/register').save($scope.user).$promise.then(function () {
+      $resource('http://127.0.0.1:8000/api/auth/register').save($scope.user).$promise.then(function () {
         toasty.pop.success(messages.REGISTRATION_SUCCESS);
         $scope.inProgress = false;
         $location.path('/login');
