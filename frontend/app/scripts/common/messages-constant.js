6b12b2ac03ddd13a7e15404f112626ee22ccff18
konradhalas <halas.konrad@gmail.com>
2014-09-24 21:46:18 +0200

    Clean up messages.

--- frontend/app/scripts/common/messages-constant.js
+++ frontend/app/scripts/common/messages-constant.js
@@ -2,11 +2,32 @@
 
 angular.module('lightningtalks')
   .constant('messages', {
-    REGISTRATION_SUCCESS: {'title': 'Success!', msg: 'Registration complete - you could now sign in.'},
-    TALK_CREATE_SUCCESS: {'title': 'Success!', msg: 'Talk added successfully.'},
-    TALK_DELETE_SUCCESS: {'title': 'Success!', msg: 'Talk deleted successfully.'},
-    TALK_UPDATE_SUCCESS: {'title': 'Success!', msg: 'Talk updated successfully.'},
-    LOGIN_SUCCESS: {'title': 'Success!', msg: 'You are logged in.'},
-    LOGOUT_SUCCESS: {'title': 'Success!', msg: 'You are logged out.'},
-    LOGIN_REQUIRED: {'title': 'Warning!', msg: 'You need to be logged in.'}
+    REGISTRATION_SUCCESS: {
+      title: 'Success!',
+      msg: 'Registration complete - you could now sign in.'
+    },
+    TALK_CREATE_SUCCESS: {
+      title: 'Success!',
+      msg: 'Talk added successfully.'
+    },
+    TALK_DELETE_SUCCESS: {
+      title: 'Success!',
+      msg: 'Talk deleted successfully.'
+    },
+    TALK_UPDATE_SUCCESS: {
+      title: 'Success!',
+      msg: 'Talk updated successfully.'
+    },
+    LOGIN_SUCCESS: {
+      title: 'Success!',
+      msg: 'You are logged in.'
+    },
+    LOGOUT_SUCCESS: {
+      title: 'Success!',
+      msg: 'You are logged out.'
+    },
+    LOGIN_REQUIRED: {
+      title: 'Warning!',
+      msg: 'You need to be logged in.'
+    }
   });
