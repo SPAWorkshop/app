aba3637c3aa27ead48176a2d40406c1bdce56b98
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2014-10-18 12:56:15 +0200

    Simplified import usage

--- backend/lightningtalks/urls.py
+++ backend/lightningtalks/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns
 from django.conf.urls import url, include
-from talks import views as talks_views
+import talks.views
 
 # TODO: TASK 1 - SESSION DETAILS
 # - configure routing
 
 urlpatterns = patterns('',
-    url(r'^api/sessions$', talks_views.SessionListView.as_view()),
-    url(r'^api/talks/(?P<pk>\d+)$', talks_views.TalkUpdateDestroyView.as_view()),
-    url(r'^api/talks$', talks_views.TalkListCreateView.as_view()),
+    url(r'^api/sessions$', talks.views.SessionListView.as_view()),
+    url(r'^api/talks/(?P<pk>\d+)$', talks.views.TalkUpdateDestroyView.as_view()),
+    url(r'^api/talks$', talks.views.TalkListCreateView.as_view()),
     url(r'^api/auth/', include('djoser.urls')),
 )
